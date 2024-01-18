6803182f82ae33fd718710be2f76f286aad4887f
Ayushtiwari101 <ayush.tiwari@kalvium.community>
2024-01-18 17:42:15 +0530

    added js

--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
+import questions from '../questions';
+ 
 
 export default function QuestionBox() {
+  const [isHighlighted, setIsHighlighted] = useState(false);
+
+  const handleHighlight = () => {
+    setIsHighlighted(true);
+  };
+
+  const handleRemoveHighlight = () => {
+    setIsHighlighted(false);
+  };
+
   return (
     <div>
-      
+      <div className={`Question-container ${isHighlighted ? 'highlighted' : ''}`}>
+        <h2>Question 1 out of 5</h2>
+        <h1 className={isHighlighted ? 'highlighted-text' : ''}>What is ReactJS?</h1>
+        <button onClick={handleHighlight}>Highlight</button>
+        <button onClick={handleRemoveHighlight}>Remove Highlight</button>
+      </div>
     </div>
-  )
+  );
 }
