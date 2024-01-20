fd44c3cccefccea81114bd84364024b83f616ff1
Ayushtiwari101 <ayush.tiwari@kalvium.community>
2024-01-20 17:04:01 +0530

    final push

--- src/App.js
+++ src/App.js
@@ -1,54 +1,31 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import questions from "./questions";
-import Result from "./components/Result";
 import QuestionBox from "./components/QuestionBox";
-import backgroundLight from './image/1000_F_69483741_ImrrZwYAsblea4pO5I9LGrNHrcH9xpy6.jpg'
-import backgroundDark from './image/imageedit_8_2298207938.jpg'
-
-
+import backgroundLight from './image/1000_F_69483741_ImrrZwYAsblea4pO5I9LGrNHrcH9xpy6.jpg';
+import backgroundDark from './image/imageedit_8_2298207938.jpg';
 
 function App() {
+  const [theme, setTheme] = useState("light");
+  const [background, setBackground] = useState(backgroundLight);
+  const [active, setActive] = useState("Light Mode");
 
-  const [theme,setTheme] = useState("light")
-  const [background,setBackground] = useState(backgroundLight)
-  const [mode, setmode] = useState(true);
-  const [active, setactive] = useState("Light");
-
-
-
-  const changeMode = () => {
-    if (mode) {
-      setmode((mode) => !mode);
-      setactive((active) => "Dark");
-    } else {
-      setmode((mode) => !mode);
-      setactive((active) => "Light");
-    }
+  const changeTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    setActive((prevActive) => (prevActive === "Light Mode" ? "Dark Mode" : "Light Mode"));
   };
 
-
-  function changeTheme(){
-    if(theme == "light"){
-      setTheme("dark")
-    }
-    else{
-      setTheme("light")
-    }
-    console.log(theme)
-  }
-
   useEffect(() => {
-    setBackground(theme == "dark" ? backgroundDark : backgroundLight)
-  
-  },[theme])
-
+    setBackground(theme === "dark" ? backgroundDark : backgroundLight);
+  }, [theme]);
 
   return (
     <div>
-      <img src={background} className="background-image" />
-      <button className='theme-change-button' onClick={changeTheme} id="theme"><h3>Change Theme</h3></button>
-      <QuestionBox Questions = {questions}/>
+      <img src={background} className="background-image" alt="Background" />
+      <button className='theme-change-button' onClick={changeTheme} id="mode">
+        {active}
+      </button>
+      <QuestionBox Questions={questions} />
     </div>
   );
 }
--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -1,98 +1,58 @@
 import React, { useState } from 'react';
 import Result from './Result';
 
-function QuestionBox(props){
-  const [mode, setmode] = useState(true);
-  const [active, setactive] = useState("Light");
+function QuestionBox(props) {
   const [correctQue, setcorrectQue] = useState(0);
   const [queNo, setqueNo] = useState(0);
-  const [highlight,setHighlight] = useState(false)
+  const [highlight, setHighlight] = useState(false);
 
-  const toggleTheme = {
-    backgroundColor: mode ? "grey" : "white",
-    color: mode ? "white" : "black",
-  };
-  const toggleBoxTheme = {
-    backgroundColor: mode ? "white" : "darkgrey",
-    color: mode ? "black" : "white",
-  };
-  const changeMode = () => {
-    if (mode) {
-      setmode((mode) => !mode);
-      setactive((active) => "Dark");
-    } else {
-      setmode((mode) => !mode);
-      setactive((active) => "Light");
-    }
-  };
   const CheckAnswer = (id) => {
     if (props.Questions[queNo].options[id].isCorrect) {
       setcorrectQue(correctQue + 1);
     }
-    console.log(props.Questions[queNo].options[id].isCorrect);
     setqueNo(queNo + 1);
   };
+
   const HighlightText = {
-    color: highlight ? "red":"black"
-  }
+    color: highlight ? "red" : "black",
+  };
+
   const Highlight = () => {
-    if(!highlight){
-      setHighlight(highlight => !highlight)
-    }
-  }
-  const RemHighlight = () => {
-    if(highlight){
-      setHighlight(highlight => !highlight)
-    }
-  }
+    setHighlight(!highlight);
+  };
+
   if (queNo < 5) {
     return (
-      <div className="QuestionBox" style={toggleTheme}>
-        <div className="Header">
-          <button className="modeToggle" onClick={changeMode}>
-            {active}
-          </button>
-        </div>
-        <div className="questionBox" style={toggleBoxTheme}>
+      <div className="QuestionBox">
+        <div className="questionBox">
+          <h2>QuickQuizIQ</h2>
           <h3>
             Question <span>{queNo + 1}</span> out of 5
           </h3>
-          <h1 className="question" style={HighlightText}>{props.Questions[queNo].text}</h1>
+          <h1 className="question" style={HighlightText}>
+            {props.Questions[queNo].text}
+          </h1>
           <div className="Options-box">
-            <button className="Option-button" onClick={() => CheckAnswer(0)}>
-              {props.Questions[queNo].options[0].text}
-            </button>
-            <button className="Option-button" onClick={() => CheckAnswer(1)}>
-              {props.Questions[queNo].options[1].text}
-            </button>
-            <button className="Option-button" onClick={() => CheckAnswer(2)}>
-              {props.Questions[queNo].options[2].text}
-            </button>
-            <button className="Option-button" onClick={() => CheckAnswer(3)}>
-              {props.Questions[queNo].options[3].text}
-            </button>
+            {props.Questions[queNo].options.map((option, index) => (
+              <button
+                key={index}
+                className="Option-button"
+                onClick={() => CheckAnswer(index)}
+              >
+                {option.text}
+              </button>
+            ))}
           </div>
           <div className="highlightButtons">
             <button onClick={Highlight}>Highlight</button>
-            <button onClick={RemHighlight}>Remove highlight</button>
+            <button onClick={() => setHighlight(false)}>Remove highlight</button>
           </div>
         </div>
       </div>
     );
   } else {
-    return (
-      <div className="QuestionBox" style={toggleTheme}>
-        <div className="Header">  
-          <button className="modeToggle" onClick={changeMode}>
-            {active}
-          </button>
-        </div>
-        <Result Result = {correctQue}/>
-      </div>
-    );
+    return <Result Result={correctQue} />;
   }
 }
+
 export default QuestionBox;
- 
-
-
